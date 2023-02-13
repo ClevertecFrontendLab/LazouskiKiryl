c018e0f997ad42de126a9534de1240f13f2bcec6
LazouskiKiryl <kiryllazouski@gmail.com>
2023-02-13 16:27:12 +0300

    feat: add Toast

---
+++ src/components/toast/toast.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import classNames from 'classnames';
+
+import { ReactComponent as CloseIcon } from '../../assets/icons/close-icon.svg';
+import { ReactComponent as SuccessIcon } from '../../assets/icons/success-icon.svg';
+import { ReactComponent as WarningIcon } from '../../assets/icons/warning-icon.svg';
+
+import cl from './toast.module.scss';
+
+const toastTypeIcons = {
+  warning: WarningIcon,
+  success: SuccessIcon,
+};
+
+interface ToastProps {
+  type: keyof typeof toastTypeIcons;
+  message: string;
+}
+
+export const Toast: FC<ToastProps> = ({ type, message }) => {
+  const Icon = toastTypeIcons[type];
+
+  return (
+    <div
+      className={classNames(cl.toast, {
+        [cl.toast_success]: type === 'success',
+        [cl.toast_warning]: type === 'warning',
+      })}
+    >
+      <Icon className={cl.icon} />
+      <p className={cl.message}>{message}</p>
+      <CloseIcon className={cl.close} />
+    </div>
+  );
+};
